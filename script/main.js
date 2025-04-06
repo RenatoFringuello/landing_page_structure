0badb71f4a00f867eb2a8028c51a1c31aa1b00bc
RenatoFringuello <renatofringuello@hotmail.it>
2025-04-07 00:26:02 +0200

    aggiunta la galleria bento, e la math utils

--- script/main.js
+++ script/main.js
@@ -7,12 +7,16 @@ window.onload = (e) => {
     })
     
     /* GALLERY */
-    loadImages()
+    const rowGallery = document.querySelector('section#gallery .row')
+    if (rowGallery){
+        // loadImages(rowGallery, 10)// to remove
+        loadBentoGallery(rowGallery, 10)
+    }
     
     /* FOOTER */
     const footerCredits = document.getElementById("credits")
     footerCredits.innerHTML = `&copy;${new Date().getFullYear().toString()} Renato Fringuello`
     
-    /* LANG */
+    /* LANG, è alla fine perché tutti i tag devono essere caricati prima di lavorarci*/
     load_language('it')
 }
